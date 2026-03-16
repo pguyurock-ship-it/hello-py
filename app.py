9153a2d76dab50e737edd07da0f4c608f0a09643
pguyurock-ship-it <pguyurock@gmail.com>
2026-03-16 21:06:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('This is my project in python')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Привет, мир! Мое приложение теперь на Hugging Face! 🚀"
+
+# Убедись, что приложение слушает порт 7860, который ожидает Hugging Face [citation:5]
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7860)
